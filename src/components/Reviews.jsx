2474f58dd6e3b651dc08e715d1063a476026a756
Stacey <stacey6105@gmail.com>
2023-02-09 15:31:32 +0000

    import error from context in reviews

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,19 +1,22 @@
 import ReviewCard from "./ReviewCard";
 import Loading from "./Loading";
 import { useParams } from "react-router-dom";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { getReviews } from "../api";
+import { ErrorContext } from "../contexts/Error";
 
 function Reviews() {
     const {category} = useParams()
     const [reviews, setReviews] = useState([]);
     const [reviewsLoading, setReviewsLoading] = useState(true)
-        useEffect(() => {
-            getReviews(category).then((reviews) => {
-                setReviews(reviews)
-                setReviewsLoading(false)
-            })
-        }, [category])
+    const {error, setError, showModal, setShowModal} = useContext(ErrorContext)
+
+    useEffect(() => {
+        getReviews(category).then((reviews) => {
+            setReviews(reviews)
+            setReviewsLoading(false)
+        })
+    }, [category])
 
     if (!reviewsLoading) {
         return (
